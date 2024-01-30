0c5153d5610b5b66b64a7476aa7581d9e93c87c2
Amulya Baral <amulya.baral@nmbu.no>
2024-01-30 15:34:51 +0100

    adjusted megahit params

--- megahit_script.sh
+++ megahit_script.sh
@@ -14,5 +14,6 @@ for forward_read in $(cat forward_reads.txt); do
     reverse_read="${forward_read/_1/_2}"
     sample_name="$(basename "$forward_read" _1.fq.gz)"
     echo -e "Now processing \n Forward read: $forward_read \n Reverse read: $reverse_read \nSample name is $sample_name"
-    megahit -1 $forward_read -2 $reverse_read -o /mnt/project/PLASTPATH/megahit_output -t 32 --min-contig-len 1000
+    megahit -1 $forward_read -2 $reverse_read -o /mnt/project/PLASTPATH/megahit_output -t 32 --min-contig-len 1000 \
+    --k-list 29,49,69,109,129,149
 done

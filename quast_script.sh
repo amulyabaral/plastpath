4c5d9f3ba8daf790b3a5d447bfcb10a90b463714
Amulya Baral <amulya.baral@nmbu.no>
2024-03-15 10:12:20 +0100

    updated quast scsript

---
+++ quast_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --ntasks=64               
+#SBATCH --job-name=assembly_plastpath   # sensible name for the job
+#SBATCH --mem=200G                 # Default memory per CPU is 3GB.
+#SBATCH --partition=hugemem-avx2     # Use the smallmem-partition for jobs requiring < 10 GB RAM.
+#SBATCH --mail-user=amulya.baral@nmbu.no # Email me when job is done.
+#SBATCH --mail-type=ALL
+
+labels=$(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa -exec dirname {} \; | xargs -I{} basename {} | tr '\n' ',')
+labels="${labels%,}"
+/mnt/project/PLASTPATH/quast-5.2.0/metaquast.py -o /mnt/project/PLASTPATH/metaquast_output --labels "$labels" -t 128 $(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa)
+

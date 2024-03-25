b066b463bd9c1dda808a1e26e46dc3bf4c48204b
Amulya Baral <amulya.baral@nmbu.no>
2024-03-25 14:37:21 +0100

    iterate one by one

--- quast_script.sh
+++ quast_script.sh
@@ -6,7 +6,19 @@
 #SBATCH --mail-user=amulya.baral@nmbu.no # Email me when job is done.
 #SBATCH --mail-type=ALL
 
-labels=$(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa -exec dirname {} \; | xargs -I{} basename {} | tr '\n' ',')
-labels="${labels%,}"
-/mnt/project/PLASTPATH/quast-5.2.0/metaquast.py -o /mnt/project/PLASTPATH/metaquast_output --labels "$labels" -t 128 $(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa | sed 's/,/, /g')
+ #labels=$(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa -exec dirname {} \; | xargs -I{} basename {} | tr '\n' ',')
+#labels="${labels%,}"
+#/mnt/project/PLASTPATH/quast-5.2.0/metaquast.py -o /mnt/project/PLASTPATH/metaquast_output --labels "$labels" -t 128 $(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa | sed 's/,/, /g')
+
+
+contigs_files=($(find /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa))
+
+for contigs_file in "${contigs_files[@]}"; do
+    label=$(basename $(dirname $contigs_file))
+    /mnt/project/PLASTPATH/quast-5.2.0/metaquast.py \
+        -o "/mnt/project/PLASTPATH/metaquast_output/$label" \
+        --labels "$label" \
+        -t 128 \
+        $contigs_file
+done
 

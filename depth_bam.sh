15688d12ebb7268edd32738342170e2b52922c47
Amulya Baral <amulya.baral@nmbu.no>
2024-04-02 10:13:29 +0200

    added depth summary file metabat2

---
+++ depth_bam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=depth_calculation
+#SBATCH --ntasks=16
+#SBATCH --mem=80G
+#SBATCH --partition=smallmem
+#SBATCH --mail-user=amulya.baral@nmbu.no
+#SBATCH --mail-type=ALL
+
+cd /mnt/project/PLASTPATH/bam_files/
+for bam_file in *.bam; do
+    jgi_summarize_bam_contig_depths --outputDepth "${bam_file%.bam}_depth.txt" $bam_file
+done

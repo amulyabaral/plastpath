b527068d7f5f98dc6f1a21893557289f85e47588
Amulya Baral <amulya.baral@nmbu.no>
2024-05-01 12:59:41 +0200

    prokka updated

--- prokka.sh
+++ prokka.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --ntasks=64                                # Number of cores (CPU)
+#SBATCH --ntasks=32                               # Number of cores (CPU)
 #SBATCH --job-name=prokka                  # Job name 
-#SBATCH --mem=200G                                  # Required RAM - Default memory per CPU is 3GB.
+#SBATCH --mem=128G                                  # Required RAM - Default memory per CPU is 3GB.
 #SBATCH --partition=hugemem                         # Use smallmem for jobs < 10 GB RAM|Options: hugemen, smallmem, gpu
 #SBATCH --mail-user=amulya.baral@nmbu.no            # Email when job is done.
 #SBATCH --mail-type=ALL
 
 module load prokka
-for contig_file in /mnt/project/PLASTPATH/high_quality_bins/*.fa; do filename=$(basename -- "$contig_file" .fa); prokka --outdir /mnt/project/PLASTPATH/prokka_output/$filename --prefix $filename $contig_file; done
+for contig_file in /mnt/project/PLASTPATH/high_quality_bins/*.fa; do filename=$(basename -- "$contig_file" .fa); prokka --outdir /mnt/project/PLASTPATH/prokka_output/$filename -cpus 32 --prefix $filename $contig_file; done

00890d7eaaec2c829e37901dc0c1c346f3814400
Amulya Baral <amulya.baral@nmbu.no>
2024-05-01 17:10:26 +0200

    new

--- prokka.sh
+++ prokka.sh
@@ -7,12 +7,24 @@
 #SBATCH --mail-user=amulya.baral@nmbu.no            # Email when job is done.
 #SBATCH --mail-type=ALL
 
+#!/bin/bash
+# Load the Prokka module
 module load prokka
 
-for contig_file in /mnt/project/PLASTPATH/high_quality_bins/*.fa ; 
-    do filename=$(basename -- "$contig_file" .fa); 
-    prokka --outdir /mnt/project/PLASTPATH/prokka_output/$filename --prefix $filename --metagenome $contig_file ;
+# Define the input directory containing .fa files
+input_directory="/mnt/project/PLASTPATH/high_quality_bins"
+
+# Define the output base directory
+output_base_directory="/mnt/project/PLASTPATH/prokka_output"
+
+# Loop through each .fa file in the input directory
+for contig_file in "${input_directory}"/*.fa; do
+    # Extract the filename without the extension
+    filename=$(basename -- "$contig_file" .fa)
+    
+    # Define the output directory for the current file
+    output_directory="${output_base_directory}/${filename}"
+    
+    # Run Prokka annotation tool
+    prokka --outdir "$output_directory" --prefix "$filename" --metagenome "$contig_file"
 done
-
-
-

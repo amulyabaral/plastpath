7e0cf59a38d45678e4a6403e94df662c7afe8626
Amulya Baral <amulya.baral@nmbu.no>
2024-05-16 15:09:30 +0200

    new scripts

---
+++ blast_contigs_card.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks=32                                 # Number of cores (CPU)
+#SBATCH --job-name=blasting               # Job name 
+#SBATCH --mem=128G                                 # Required RAM - Default memory per CPU is 3GB.
+#SBATCH --partition=hugemem-avx2                         # Use smallmem for jobs < 10 GB RAM|Options: hugemen, smallmem, gpu
+#SBATCH --mail-user=amulya.baral@nmbu.no            # Email when job is done.
+#SBATCH --mail-type=ALL
+
+
+for contig_file in /mnt/project/PLASTPATH/megahit_output/*/final.contigs.fa; do \
+    dir_name=$(basename $(dirname $contig_file))
+    blastn -query $contig_file \ 
+    -db /mnt/project/PLASTPATH/blast_db/card_blastn \
+    --outfmt "6 qseqid qlen sseqid sallseqid slen qstart qend sstart send evalue bitscore score length pident nident mismatch positive gapopen gaps ppos salltitles qcovs qcovhsp" \
+    -evalue 1e-30 \
+    -num_threads 32
+    -out /mnt/project/PLASTPATH/blast_output/${dir_name}_blast_output.txt ;
+done
+    

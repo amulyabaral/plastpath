dc9556f1a18605529c9235fed67face492ef43b0
Amulya Baral <amulya.baral@nmbu.no>
2024-06-17 09:25:47 +0200

    seq finding script

--- extracting_contigs.sh
+++ extracting_contigs.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-# Define the path to the folder containing your FASTA files
-FASTA_FOLDER="/mnt/project/PLASTPATH/megahit_output"
+# Path to the contig_and_file text file
+contig_file="path/to/contig_and_file.txt"
 
-# Path to the TSV file containing the names of unique contigs (qseqid)
-CONTIGS_FILE="/mnt/project/PLASTPATH/plastpath/contig_names_amr_flank.tsv"
-
-# Output file where the sequences will be pasted
-OUTPUT_FASTA="/mnt/project/PLASTPATH/amr_contigs_with_flanks.fasta"
-
-# Check if output file exists and remove if it does
-[ -f "$OUTPUT_FASTA" ] && rm "$OUTPUT_FASTA"
-
-# Find all 'final.contigs.fa' files under the specified directory
-find "$FASTA_FOLDER" -name 'final.contigs.fa' | while read fasta_file
+# Read the contig_and_file text file line by line
+while IFS= read -r line
 do
-  # Loop through each qseqid in the TSV file for each FASTA file found
-  while read -r contig
-  do
-    # Use awk to find the contig and capture until the next header
-    awk -v contig="^>$contig$" '
-      $0 ~ contig {printit = 1}
-      $0 ~ /^>/ && $0 !~ contig {printit = 0}
-      printit {print}
-    ' "$fasta_file" >> "$OUTPUT_FASTA"
-  done < "$CONTIGS_FILE"
-done
+  # Extract the qseqid (assuming it's the first column)
+  qseqid=$(echo $line | cut -d ' ' -f 1)
 
-echo "Contig extraction complete."
+  # Extract the FileName (assuming it's the second column)
+  directory_name=$(echo $line | cut -d ' ' -f 2)
+
+  # Construct the path to the FASTA file
+  fasta_file_path="/mnt/project/PLASTPATH/megahit_output/$directory_name/final_contigs.fa"
+
+  # Check if the file exists
+  if [ -f "$fasta_file_path" ]; then
+    # Use awk to search for the contig in the FASTA file
+    awk -v seq=">$qseqid" 'BEGIN {RS = ">"} $1 == seq {print ">"$0}' "$fasta_file_path"
+  else
+    echo "File not found: $fasta_file_path"
+  fi
+done < "$contig_file"
